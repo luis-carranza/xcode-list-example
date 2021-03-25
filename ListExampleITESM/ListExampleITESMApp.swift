079201c9a9e4a3977913999352110809bead92fb
Luis Carranza <carranlu@gmail.com>
2021-03-25 13:21:15 -0600

    Initial Commit

---
+++ ListExampleITESM/ListExampleITESMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListExampleITESMApp.swift
+//  ListExampleITESM
+//
+//  Created by Luis Javier Carranza on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ListExampleITESMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
