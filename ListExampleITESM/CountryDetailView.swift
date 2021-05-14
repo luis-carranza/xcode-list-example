c9a01815a651f8bf4e7c9b929f947965417ea9ea
Luis Carranza <carranlu@gmail.com>
2021-05-13 22:38:53 -0500

    Added Icons to the app

--- ListExampleITESM/CountryDetailView.swift
+++ ListExampleITESM/CountryDetailView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct CountryDetailView: View {
-    
-
-    
     let country: CountryModel
-    
     var body: some View {
-        Text(country.countryName + "  ->  Population: " + country.population)
-            .navigationBarTitle(country.countryName)
+        
+        ZStack { 
+            
+            Image("Mexico")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .overlay(
+                    CountryText(country: country.countryName, population: country.population), alignment: .center)
+        }// ZStack
     }
 }
 
 struct CountryDetailView_Previews: PreviewProvider {
     static var previews: some View {
         CountryDetailView(country: CountryModel(id: UUID(), countryName: "Canada", population: "400M"))
+    }
+}
+
+struct CountryText: View {
+    
+    let country: String
+    let population: String
+    var body: some View {
+        ZStack {
+            VStack {
+                Text(country)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 6)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                
+                
+                Text("Population: " + population)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 6)
+                    .foregroundColor(.white)
+                    .font(.callout)
+            }
+            
+        }.background(Color.black)
+        .opacity(0.8)
+        .cornerRadius(6)
+        .padding(10)
+        
     }
 }
